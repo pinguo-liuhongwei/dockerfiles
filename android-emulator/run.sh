44f54d288c4ba1d4376c12cb5270471d6bd0f645
pinguo-zhanglu <zhanglu@camera360.com>
2015-06-16 15:51:26 +0800

    update

--- android-emulator/run.sh
+++ android-emulator/run.sh
@@ -3,6 +3,10 @@
 echo '/etc/init.d/ssh start'
 /etc/init.d/ssh start
 
+echo 'run facedetect'
+/home/worker/bin/start-emulator
+adb shell am start -n us.pinguo.facedetect/us.pinguo.facedetect.MainActivity
+
 echo '/usr/local/bin/supervisord -c /home/worker/supervisor/supervisord.conf'
 /usr/local/bin/supervisord -c /home/worker/supervisor/supervisord.conf
 
