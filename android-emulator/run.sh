ce67e20d7b59589e2b84f45527af9f20de902c93
pinguo-zhanglu <zhanglu@camera360.com>
2015-06-22 00:06:01 +0800

    update

--- android-emulator/run.sh
+++ android-emulator/run.sh
@@ -3,14 +3,14 @@
 echo '/etc/init.d/sshd start'
 /etc/init.d/sshd start
 
+echo 'service crond start'
+service crond start
+
 if [ -f /home/worker/bin/init.sh ]; then
     echo '/home/worker/bin/init.sh'
     chmod a+x /home/worker/bin/init.sh
     su worker -c '/home/worker/bin/init.sh'
 fi
-
-echo 'service crond start'
-service crond start
 
 echo '/usr/bin/supervisord -c /home/worker/supervisor/supervisord.conf'
 /usr/bin/supervisord -c /home/worker/supervisor/supervisord.conf
